ab7b52c61fca414ffae0f94b2a243c95c6a878e0
Arthur Clune <arthur@clune.org>
2013-01-24 12:42:51 +0000

    Add option to log everything

--- src/PasteWatch/Alert.hs
+++ src/PasteWatch/Alert.hs
@@ -14,13 +14,16 @@ import PasteWatch.Types
 
 -- | If the given Paste includes our pattern, return the one that matched
 checkContent::[T.Text] -> [T.Text] -> PasteContents -> Maybe MatchText
-checkContent alerts alertsci (PasteContents s) =
-    case A.maybeResult $ A.feed (A.parse alertp s) T.empty of
+checkContent alerts alertsci (PasteContents s)
+  | logall    = Just (MatchText "ANY")
+  | otherwise =
+      case A.maybeResult $ A.feed (A.parse alertp s) T.empty of
                 Nothing -> Nothing
                 Just v  -> Just $ MatchText v
   where
     alertp = manyTill (A.choice matchlist) <* A.takeText
     matchlist = map A.string alerts ++ map A.asciiCI alertsci
+    logall = (alerts == ["ALL"]) && (alertsci == ["ALL"])
 
 -- | parse zero or more instances of end, skipping over one character on failure
 -- and returning the matched text if one succeeds
