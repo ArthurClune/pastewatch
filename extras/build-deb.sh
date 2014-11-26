9ad1f0d740175c35c1608ba64c9e2d85d0685fb1
Arthur Clune <arthur@clune.org>
2014-11-26 11:15:12 +0000

    Update build script for new locations of ekg files and ghc 7.8

--- extras/build-deb.sh
+++ extras/build-deb.sh
@@ -19,22 +19,24 @@ fi
 cd pastewatch
 git pull
 VERSION=`grep "^version:" pastewatch.cabal| awk '{split($0,a," "); print a[2]}' `
-# if you have installed and then removed the .deb, you may need this next line
-# to get a build next time
-#sudo cabal --reinstall --force-reinstall --datadir=/usr/share install ekg
+
+EKGVER=`grep "ekg " cabal.config |cut -f 3 -d "=" |cut -d ',' -f 1`
+EKG="ekg-${EKGVER}"
+# sudo cabal --reinstall --force-reinstall --datadir=/usr/share install $EKG
 sudo cabal --datadir=/usr/share install
 sudo cp dist/build/pastewatch/pastewatch /usr/bin/pastewatch
 sudo cp sample.config /etc/pastewatch.conf
 sudo cp extras/ubuntu_init_script.sh /etc/init.d/pastewatch
 
 sudo rm -f /usr/share/ekg-*/assets/*
-sudo cp -r assets/ekg/assets/* /usr/share/ekg-*/assets/
+sudo cp -r assets/ekg/assets/* /usr/share/x86_64-linux-ghc-7.8.3/ekg-0.4.0.3/assets
 
 cd ..
 fakeroot fpm -s dir -t deb -n "pastewatch" -v $VERSION --license="GPLv3" --vendor="clune.org" -m "arthur@clune.org" \
     --description="Watch paste sites for interesting stuff" \
     --url="https://github.com/ArthurClune/pastewatch" \
     --config-files=/etc/pastewatch.conf \
-    /usr/bin/pastewatch /etc/init.d/pastewatch /etc/pastewatch.conf /usr/share/ekg-*
+    /usr/bin/pastewatch /etc/init.d/pastewatch /etc/pastewatch.conf \
+    /usr/share/x86_64-linux-ghc-7.8.3/$EKG/assets
 
 
